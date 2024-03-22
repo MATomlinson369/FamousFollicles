2f64489124852109a54b4cc36d6572f89f447e14
MATomlinson369 <152192076+MATomlinson369@users.noreply.github.com>
2024-03-22 14:43:27 +0100

    Add files via upload

---
+++ pitt.js
@@ -0,0 +1,132 @@
+document.querySelector("h1").textContent = "Famous Follicles"
+document.querySelector("h2").textContent = "From bald to bold!"
+
+
+// bron code: chat gpt & hulp van Carmen Zoutendijk
+// code bron: ChatGPT: prompt; how do i make a audio loop mute?
+document.getElementById("mutemuziek").addEventListener("click",function(){
+    let audio = document.getElementById("muziekje");
+audio.muted = !audio.muted;
+this.textContent = audio.muted ? "unmute" : "mute";
+} ) ;
+// ----
+
+let huidigeFoto = document.getElementById("character")
+
+
+const hair1knop = document.querySelector("#hair1")
+const hair2knop = document.querySelector("#hair2")
+const hair3knop = document.querySelector("#hair3")
+const hair4knop = document.querySelector("#hair4")
+
+
+const showTimepitt = document.querySelector("#showtimebtn2")
+
+
+
+
+let hasHair = false
+
+
+
+
+// bron voor endswith: youtube tutorial voor image slider: https://youtu.be/749ta0nvj8s?si=7MUBc705D6AdI7pY
+
+function veranderHaar1(){
+    
+    if(huidigeFoto.src.endsWith("rock.png")){
+        huidigeFoto.src ="images/haar1-rock.png";
+    }
+    else if (huidigeFoto.src.endsWith("phil.png")){
+        huidigeFoto.src ="images/haar1-phil.png";
+    }
+
+    else {
+        huidigeFoto.src = "images/haar1-pitt.png";
+    }
+
+    hasHair = true
+
+    
+}
+
+function veranderHaar2(){
+    if(huidigeFoto.src.endsWith("rock.png")){
+        huidigeFoto.src ="images/haar2-rock.png";
+    }
+
+    else if (huidigeFoto.src.endsWith("phil.png")){
+        huidigeFoto.src ="images/haar2-phil.png";
+    }
+    else {
+        huidigeFoto.src = "images/haar2-pitt.png";
+    }
+
+    hasHair = true
+}
+
+function veranderHaar3(){
+    if(huidigeFoto.src.endsWith("rock.png")){
+        huidigeFoto.src ="images/haar3-rock.png";
+    }
+    else if (huidigeFoto.src.endsWith("phil.png")){
+        huidigeFoto.src ="images/haar3-phil.png";
+    }
+    else {
+        huidigeFoto.src = "images/haar3-pitt.png";
+    }
+    hasHair = true
+}
+
+function veranderHaar4(){
+    if(huidigeFoto.src.endsWith("rock.png")){
+        huidigeFoto.src ="images/haar4-rock.png";
+    }
+    else if (huidigeFoto.src.endsWith("phil.png")){
+        huidigeFoto.src ="images/haar4-phil.png";
+    }
+    else {
+        huidigeFoto.src = "images/haar4-pitt.png";
+    }
+    hasHair = true
+    
+}
+// ----
+
+
+function showHappypitt(){
+
+    if (hasHair) 
+            if(huidigeFoto.src.endsWith("haar1-pitt.png")){
+            huidigeFoto.src ="images/haar1.1-pitt.png"
+            }
+            else if (huidigeFoto.src.endsWith("haar2-pitt.png")){
+            huidigeFoto.src ="images/haar2.1-pitt.png" }
+            else if (huidigeFoto.src.endsWith("haar3-pitt.png")){
+                huidigeFoto.src ="images/haar3.1-pitt.png" }
+        
+            else {
+            huidigeFoto.src ="images/haar4.1-pitt.png"
+            }
+            
+        }
+
+
+    
+
+hair1knop.addEventListener("click", veranderHaar1)
+hair2knop.addEventListener("click", veranderHaar2)
+hair3knop.addEventListener("click", veranderHaar3)
+hair4knop.addEventListener("click", veranderHaar4)
+
+
+showTimepitt.addEventListener("click",showHappypitt)
+
+
+
+
+
+
+
+
+
